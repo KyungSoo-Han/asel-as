804524de64a928ad951199398b072abb0ff8ed92
hanks <hanks6160@gmail.com>
2022-12-26 22:25:23 +0900

    Initial commit

---
+++ src/main/java/kr/co/biztax/aselas/AselAsApplication.java
@@ -0,0 +1,13 @@
+package kr.co.biztax.aselas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AselAsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AselAsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/biztax/aselas/AselAsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.biztax.aselas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AselAsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
